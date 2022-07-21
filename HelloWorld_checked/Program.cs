caf829490d7160b4eb9952e4867349cf7603b3c4
soumany <soumany06@gmail.com>
2022-07-21 17:58:31 +0700

    commit

---
+++ HelloWorld_checked/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld_checked
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int FirstInteger;
+            int SecondInteger;      
+            Console.WriteLine("integers");
+            Console.Write("Type the 1 interger");
+            FirstInteger = int.Parse(Console.ReadLine());
+            Console.Write("Type the 2 integer");
+            SecondInteger = int.Parse(Console.ReadLine());
+
+            
+            // a program to add 2 integers
+            /*
+             * 
+             * 
+             */
+        }
+    }
+}
