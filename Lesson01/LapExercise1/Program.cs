4dafa5b5eb46568ef7e97b0ea69b160a15ddb836
Many <soumany06@gmail.com>
2022-07-23 14:38:46 +0700

    commit

---
+++ Lesson01/LapExercise1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LapExercise1
+{
+    class Car
+    {
+        public string strModel;
+        public string strBrand;
+        public float fltPrice;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car objCar1; //part b declare an object name of type car
+            objCar1 = new Car(); // part c
+            //Car objcar = new Car();
+            Console.Write("what is teh model of your car:");
+            objCar1.strModel = Console.ReadLine();
+            Console.Write("what is teh brand  of your car:");
+            objCar1.strBrand = Console.ReadLine();
+            Console.Write("what is teh price of your car:");
+            objCar1.fltPrice = float.Parse(Console.ReadLine());
+            Console.WriteLine("Car Model is {0} , Brand car is {1} and Price is {2}", objCar1.strModel, objCar1.strBrand, objCar1.fltPrice);
+
+
+        }
+    }
+}
