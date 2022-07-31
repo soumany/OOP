470f11248e155b05e3b71c0c0e6d4916874ba41a
Many <soumany06@gmail.com>
2022-07-31 13:57:30 +0700

    commit

---
+++ My Code/My Code/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_Code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Enter your age");
+            int Age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your height");
+            float Height = float.Parse(Console.ReadLine());
+            Console.WriteLine("Your name is {0}. Your age is {1}.Your height is {2}",Name, Age, Height); 
+
+        }
+    }
+}
