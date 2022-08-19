ed556bb6ebf55660707146dc22c60b0b0eeadefc
soumany <soumany06@gmail.com>
2022-08-19 16:14:59 +0700

    commit

---
+++ Test2/Test2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2
+{
+    class House
+    {
+        public string strType;
+        private string strAddress;
+        private float fltPrice;
+        public string Address
+        {
+            get { return strAddress; }
+            set { strAddress = value; }
+
+        }
+        public float Price
+        { get { return fltPrice; }
+          set { if (value>0)fltPrice = value; }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            House hObj = new House();
+            Console.WriteLine("Enter the type of house:");
+            hObj.strType = Console.ReadLine();
+            Console.WriteLine("enter the address:");
+            hObj.Address = Console.ReadLine();
+            Console.WriteLine("Enter the price:");
+            hObj.Price = float.Parse(Console.ReadLine());
+            Console.WriteLine("Type {0} Address {1} Price {2}",
+                              hObj.strType, hObj.Address, hObj.Price);        }
+    }
+}
